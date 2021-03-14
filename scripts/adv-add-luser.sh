eba3d4140a39f1010390d326c9dcb0c9620be4af
Dustin K Greco <dkgreco.dev@gmail.com>
2021-03-14 15:41:30 -0700

    Created script for advanced user additions to system

--- scripts/adv-add-luser.sh
+++ scripts/adv-add-luser.sh
@@ -6,6 +6,7 @@
 # Passwords will be automatically generated for the account.
 # This script implements message suppression.
 # Finally, user account information will be printed to the console.
+CONTACT_SYS_ADMIN=0
 
 # Check for superuser status
 if [[ "${UID}" -ne 0 ]]
@@ -40,14 +41,19 @@ then
 fi
 
 # Generate a random password
-PASSWORD=$(date %s%N | sha256sum | head -c 48)
+KEY_FRAGMENT_1=$(date +%s%N) >&2
+RANDOM_NUM=$(((RANDOM * 25) + 1 )) >&2
+KEY_FRAGMENT_2=$(head -c ${RANDOM_NUM} </dev/urandom) >&2
+KEY_FRAGMENT_3="${RANDOM}${RANDOM}${RANDOM}${RANDOM}" >&2
+FORGED_KEY="${KEY_FRAGMENT_3}${KEY_FRAGMENT_1}${KEY_FRAGMENT_2}" >&2
+PASSWORD=$(echo "${FORGED_KEY}" | sha256sum | head -c 48) >&2
 
 # Set password for the user account, or, Log Error
 echo "${PASSWORD}" | passwd --stdin ${USER_NAME} &> /dev/null
 if [[ "${?}" -ne 0 ]]
 then
-  echo "Unable to create the user account." >&2
-  echo 'Manually create the user account, or, submit a ticket to the Sys-Admin Team' >&2
+  echo "Unable to set the password for the user account." >&2
+  echo 'Manually set the password, or, submit a ticket to the Sys-Admin Team' >&2
   echo 'If you submit a ticket to the Sys-Admin Team, please allow 48 hours for ticket resolution.' >&2
   echo 'If the Sys-Admin Team cannot meet the 48 hour deadline, you will receive a notification with a set deadline' >&2
   exit 1
@@ -58,11 +64,12 @@ passwd -e ${USER_NAME} &> /dev/null
 
 # Print Info To Console
 echo
-echo 'Username: '
+echo 'Username:'
 echo "${USER_NAME}"
 echo
-echo 'Password: '
+echo 'Password:'
 echo "${PASSWORD}"
 echo
-echo 'Host'
+echo 'Hostname:'
 echo "${HOSTNAME}"
+exit 0
